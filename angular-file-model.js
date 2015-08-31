d80ed5de5bf213c8250e5d5c809a11aeb35c7522
Yoza Wiratama <Yoza Wiratama>
2015-09-01 01:38:01 +0700

    first commit
    ready to use angula file model

---
+++ angular-file-model.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular.module('fileread', [])
+
+    .directive("fileread", [function () {
+        return {
+            scope: {
+                fileread: "="
+            },
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            scope.fileread = {
+                                lastModified: changeEvent.target.files[0].lastModified,
+                                lastModifiedDate: changeEvent.target.files[0].lastModifiedDate,
+                                name: changeEvent.target.files[0].name,
+                                size: changeEvent.target.files[0].size,
+                                type: changeEvent.target.files[0].type,
+                                data: loadEvent.target.result
+                            };
+                        });
+                    }
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }]);
+
+})();
